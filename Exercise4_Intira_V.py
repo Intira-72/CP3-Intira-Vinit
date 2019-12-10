b398fbb8c122a07f9c62af63157efc6a3181d901
Intira-72 <58544293+Intira-72@users.noreply.github.com>
2019-12-10 11:28:55 +0700

    Upload Exercise 4

---
+++ Exercise4_Intira_V.py
@@ -0,0 +1,10 @@
+fe = 30
+gb = 40
+ics = 35
+cp = 42
+
+print("--- Your Score ---")
+print("Foundation English : ", fe)
+print("General Business : ", gb)
+print("Introduction to Computer Systems : ", ics)
+print("Computer Programming : ", cp)
